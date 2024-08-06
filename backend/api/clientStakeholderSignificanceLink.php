50591dfbbc0065ffe0812b5df08f551c7d51dca7
E-AdisBabic <adis.babic@e-infra.com>
2024-08-06 11:33:56 +0200

    - SubStekeholder responded set to 1, after ansering survey

--- backend/api/clientStakeholderSignificanceLink.php
+++ backend/api/clientStakeholderSignificanceLink.php
@@ -8,66 +8,66 @@
  * 
  *  09.07.2024  JM
  * 
-*/
+ */
 
 $notLog = 1;
 
-include( 'inc/include.php' );
+include('inc/include.php');
 require_once __DIR__ . '/../inc/all.php';
 
-$param = json_decode( $_REQUEST[ 'param' ] );
+$param = json_decode($_REQUEST['param']);
 
 $jsonArray = array();
 
-if( isset( $param->action ) )
+if (isset($param->action))
     $action = $param->action;
 else
     $action = 'r';
 
 $clientStakeholderSignificanceId = $param->clientStakeholderSignificanceId;
 
-$cols = array('clientStakeholderSignificanceId' => $clientStakeholderSignificanceId );
+$cols = array('clientStakeholderSignificanceId' => $clientStakeholderSignificanceId);
 
 $query = 'SELECT * FROM `wa_clientStakeholderSignificance` WHERE active = 1 AND id = :clientStakeholderSignificanceId';
 $clientStakeholderSignificance = dbSelect($db, $query, $cols);
 
-if ( count($clientStakeholderSignificance ) == 0 ) {
+if (count($clientStakeholderSignificance) == 0) {
     $jsonArray = array();
-    $jsonArray[ 'errorNo' ] = 2;
-    $jsonArray[ 'errorMessage' ] = 'kein Eintrag';
+    $jsonArray['errorNo'] = 2;
+    $jsonArray['errorMessage'] = 'kein Eintrag';
     $action = "x";
 }
 
 
-switch ( $action ) {
+switch ($action) {
 
     case "r":                                                                                                                                       // read
-        $cols = array('clientStakeholderSignificanceId' => $clientStakeholderSignificanceId );
+        $cols = array('clientStakeholderSignificanceId' => $clientStakeholderSignificanceId);
 
         $query = 'SELECT * FROM `wa_clientStakeholderSignificanceLinks` WHERE active = 1 AND clientStakeholderSignificanceId = :clientStakeholderSignificanceId';
         $clientStakeholderSignificanceLinks = dbSelect($db, $query, $cols);
 
-ar( $clientStakeholderSignificanceLinks );
+        ar($clientStakeholderSignificanceLinks);
 
-        if( count( $clientStakeholderSignificanceLinks ) > 1) {
+        if (count($clientStakeholderSignificanceLinks) > 1) {
             $pointer = 0;
             $jsonArray = array();
-            foreach( $clientStakeholderSignificanceLinks as $clientStakeholderSignificanceLink ) {
-                $jsonArray[ $pointer ][ 'id' ] = $clientStakeholderSignificanceLink[ 'id' ];
-   
-                $jsonArray[ $pointer ][ "link" ][ 'title' ] = $clientStakeholderSignificanceLink[ 0 ][ 'title' ];
-                $jsonArray[ $pointer ][ "link" ][ 'url' ] = $clientStakeholderSignificanceLink[ 0 ][ 'link' ];
-                $jsonArray[ $pointer ][ "link" ][ 'text' ] = $clientStakeholderSignificanceLink[ 0 ][ 'text' ];
-                $jsonArray[ $pointer ][ "link" ][ 'editDate' ] = $clientStakeholderSignificanceLink[ 0 ][ 'editDate' ];
+            foreach ($clientStakeholderSignificanceLinks as $clientStakeholderSignificanceLink) {
+                $jsonArray[$pointer]['id'] = $clientStakeholderSignificanceLink['id'];
+
+                $jsonArray[$pointer]["link"]['title'] = $clientStakeholderSignificanceLink[0]['title'];
+                $jsonArray[$pointer]["link"]['url'] = $clientStakeholderSignificanceLink[0]['link'];
+                $jsonArray[$pointer]["link"]['text'] = $clientStakeholderSignificanceLink[0]['text'];
+                $jsonArray[$pointer]["link"]['editDate'] = $clientStakeholderSignificanceLink[0]['editDate'];
                 $pointer++;
             }
         }
 
-ar( $jsonArray );
+        ar($jsonArray);
 
-exit;
+        exit;
 
-/*
+        /*
 
             $jsonArray = array();
 
@@ -85,8 +85,8 @@ exit;
             $jsonArray[ 'errorMessage' ] = 'kein Eintrag';
         }
 
-*/        
-        break;
+*/
+        // break;
 
 
 
@@ -103,55 +103,53 @@ exit;
 
         $query = 'SELECT * FROM `wa_clientStakeholderSignificance` WHERE active = 1 AND clientId = :clientId AND clientSubgroupId = :clientSubGroupId AND clientStakeholderId = :clientStakeholderId';
         $clientStakeholderSignificance = dbSelect($db, $query, $cols);
-        if( count( $clientStakeholderSignificance ) > 0 ) {
+        if (count($clientStakeholderSignificance) > 0) {
             $jsonArray = array();
-            $jsonArray[ 'errorNo' ] = 3;
-            $jsonArray[ 'errorMessage' ] = 'Datensatz schon vorhanden';
-        }
-        else {
+            $jsonArray['errorNo'] = 3;
+            $jsonArray['errorMessage'] = 'Datensatz schon vorhanden';
+        } else {
             $cols = array('clientId' => $clientId);
             $cols['clientSubGroupId'] = $clientSubGroupId;
             $cols['clientStakeholderId'] = $clientStakeholderId;
-            $cols[ 'active' ] = 1; 
+            $cols['active'] = 1;
 
             $lastId = dbInsert($db, 'wa_clientStakeholderSignificance', $cols);
             $jsonArray = array();
-            $jsonArray[ 'lastId' ] = $lastId;
-            
-            $cols = array('clientStakeholderSignificanceId' => $lastId );
-            $cols[ 'active' ] = 1; 
-            $cols[ 'title' ] = $title;
-            $cols[ 'text' ] = $text;
-            $cols[ 'editDate' ] = date("Y-m-d H:i:s");
+            $jsonArray['lastId'] = $lastId;
+
+            $cols = array('clientStakeholderSignificanceId' => $lastId);
+            $cols['active'] = 1;
+            $cols['title'] = $title;
+            $cols['text'] = $text;
+            $cols['editDate'] = date("Y-m-d H:i:s");
             $lastId = dbInsert($db, 'wa_clientStakeholderSignificanceText', $cols);
-        }      
+        }
         break;
-   
+
     case "d":                                                                                                                                       // delete
         $clientStakeholderSignificanceId = $param->clientStakeholderSignificanceId;                                                                                                                                         // delete
         $deleteMode = $param->deleteMode;
 
-        $cols = array('clientStakeholderSignificanceId' => $clientStakeholderSignificanceId );
+        $cols = array('clientStakeholderSignificanceId' => $clientStakeholderSignificanceId);
         $query = 'SELECT * FROM `wa_clientStakeholderSignificance` WHERE active = 1 AND id = :clientStakeholderSignificanceId';
         $clientStakeholderSignificance = dbSelect($db, $query, $cols);
 
         if (count($clientStakeholderSignificance) == 1) {
             $cols = array();
-            $cols[ 'active' ] = 0;       
-            dbUpdate( $db, 'wa_clientStakeholderSignificance', $clientStakeholderSignificanceId, $cols );
+            $cols['active'] = 0;
+            dbUpdate($db, 'wa_clientStakeholderSignificance', $clientStakeholderSignificanceId, $cols);
 
             $cols = array();
-            $cols[ 'clientStakeholderSignificanceId' ] = $clientStakeholderSignificanceId;   
-            $query = 'update wa_clientStakeholderSignificanceText set active = 0 where clientStakeholderSignificanceId = :clientStakeholderSignificanceId'; 
-            dbQuery($db, $query, $cols );
-  
-            $jsonArray[ 'return' ] = 1;   
+            $cols['clientStakeholderSignificanceId'] = $clientStakeholderSignificanceId;
+            $query = 'update wa_clientStakeholderSignificanceText set active = 0 where clientStakeholderSignificanceId = :clientStakeholderSignificanceId';
+            dbQuery($db, $query, $cols);
+
+            $jsonArray['return'] = 1;
+        } else {
+            $jsonArray['return'] = 0;
         }
-        else {
-            $jsonArray[ 'return' ] = 0;   
-        }   
         break;
-        
+
 
     case "e":                                                                                                                                         // edit
         $clientStakeholderSignificanceId = $param->clientStakeholderSignificanceId;
@@ -159,24 +157,23 @@ exit;
         $text = $param->text;
 
         $cols = array();
-        $cols[ 'clientStakeholderSignificanceId' ] = $clientStakeholderSignificanceId;
+        $cols['clientStakeholderSignificanceId'] = $clientStakeholderSignificanceId;
 
         $query = 'SELECT * FROM `wa_clientStakeholderSignificanceText` WHERE active = 1 AND clientStakeholderSignificanceId = :clientStakeholderSignificanceId';
         $clientStakeholderSignificance = dbSelect($db, $query, $cols);
-        if( count( $clientStakeholderSignificance ) > 0 ) {
-            $cols[ 'title' ] = $title;
-            $cols[ 'text' ] = $text;
-            $cols[ 'editDate' ] = date("Y-m-d H:i:s");
-            dbUpdate( $db, 'wa_clientStakeholderSignificanceText',  $clientStakeholderSignificance[ 0 ][ 'id' ] , $cols);
-            
-            $jsonArray[ 'return' ] = 1;   
-        }
-        else {
+        if (count($clientStakeholderSignificance) > 0) {
+            $cols['title'] = $title;
+            $cols['text'] = $text;
+            $cols['editDate'] = date("Y-m-d H:i:s");
+            dbUpdate($db, 'wa_clientStakeholderSignificanceText',  $clientStakeholderSignificance[0]['id'], $cols);
+
+            $jsonArray['return'] = 1;
+        } else {
 
             $jsonArray = array();
-            $jsonArray[ 'errorNo' ] = 2;
-            $jsonArray[ 'errorMessage' ] = 'kein Eintrag';
-        }      
+            $jsonArray['errorNo'] = 2;
+            $jsonArray['errorMessage'] = 'kein Eintrag';
+        }
         break;
 
     case "x":
@@ -184,10 +181,8 @@ exit;
 
     default:
         $jsonArray = array();
-        $jsonArray[ 'errorNo' ] = 2;
-        $jsonArray[ 'errorMessage' ] = 'unbekannte Aktion';
-    }        
-      
-    echo json_encode( $jsonArray );
+        $jsonArray['errorNo'] = 2;
+        $jsonArray['errorMessage'] = 'unbekannte Aktion';
+}
 
-?>
+echo json_encode($jsonArray);
